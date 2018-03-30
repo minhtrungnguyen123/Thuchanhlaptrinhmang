2671aa2a269cf886fc4f3458bd437649b6df27bd
NguyenMinhTrung <nguyenminhtrung249@gmail.com>
2018-03-30 23:24:09 +0700

    Add new file THBuoi4.cs

---
+++ THBuoi4.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace UDP
+{
+    class Program
+    {
+        //code thuc hanh buoi 4 UDP
+        static void Main(string[] args)
+        {
+            string IP = "192.168.1.25";
+            int port = 80;
+
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+
+            Thread receiveThread = new Thread(ReceiveData);
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+
+            UdpClient client = new UdpClient();
+
+            try
+            {
+                string text;
+                do
+                {
+                    text = Console.ReadLine();
+
+                    if (text.Length != 0)
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes(text);
+                        client.Send(data, data.Length, remoteEndPoint);
+                    }
+                } while (text.Length != 0);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private static void ReceiveData()
+        {
+            UdpClient client = new UdpClient(80);
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] data = client.Receive(ref anyIP);
+                    string text = Encoding.UTF8.GetString(data);
+                    Console.WriteLine("--> " + text);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.ToString());
+                }
+            }
+        }
+    }
+}
